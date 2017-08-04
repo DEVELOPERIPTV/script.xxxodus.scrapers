82d97a82e2a1d57d0fc94ad4c327794f07044a52
Echo Coder <echocoderxbmc@protonmail.com>
2017-08-04 22:55:03 +0100

    Bump

--- lib/scrapers/sexvr.py
+++ lib/scrapers/sexvr.py
@@ -1,4 +1,4 @@
-import xbmc,xbmcplugin,os,urlparse,re
+import xbmc,xbmcplugin,os,urlparse,re,base64
 import client
 import kodi
 import dom_parser2
@@ -6,33 +6,33 @@ import log_utils
 import scraper_updater
 from resources.lib.modules import utils
 from resources.lib.modules import helper
-from resources.lib.modules import linkfinder
 buildDirectory = utils.buildDir
 
 filename     = os.path.basename(__file__).split('.')[0]
-base_domain  = 'http://www.streamingporn.xyz'
+base_domain  = 'https://www.sexvr.com'
 base_name    = base_domain.replace('www.',''); base_name = re.findall('(?:\/\/|\.)([^.]+)\.',base_name)[0].title()
-type         = 'scenes'
-menu_mode    = 282
-content_mode = 283
-player_mode  = 810
+type         = 'vr'
+menu_mode    = 288
+content_mode = 289
+player_mode  = 801
 
 search_tag   = 1
-search_base  = urlparse.urljoin(base_domain,'?s=%s')
+search_base  = urlparse.urljoin(base_domain,'search/videos/%s/')
 
 @utils.url_dispatcher.register('%s' % menu_mode)
 def menu():
     
     scraper_updater.check(filename)
-    
+
     try:
-        url = urlparse.urljoin(base_domain,'brittany-shae-shows-us-shes-got-bangcasting-combangbros-com/')
+        url = urlparse.urljoin(base_domain,'channels/')
         c = client.request(url)
-        r = dom_parser2.parse_dom(c, 'div', {'class': 'textwidget'})
-        r = dom_parser2.parse_dom(r, 'p')
-        r = [i.content for i in r if 'Popular Categories' in i.content]
-        r = dom_parser2.parse_dom(r, 'a')
-        r = [(i.attrs['href'], i.content) for i in r]
+        r = dom_parser2.parse_dom(c, 'div', {'class': ['item-col','item--channel','col']})
+        r = [(dom_parser2.parse_dom(i, 'a', req=['href','title']), \
+              dom_parser2.parse_dom(i, 'img', req='src')) \
+              for i in r if i]
+        r = [(i[0][0].attrs['href'], i[0][0].attrs['title'], i[1][0].attrs['src'] \
+            if i[1] else urlparse.urljoin(base_domain,'core/images/catdefault.jpg')) for i in r[8:]]
         if ( not r ):
             log_utils.log('Scraping Error in %s:: Content of request: %s' % (base_name.title(),str(c)), log_utils.LOGERROR)
             kodi.notify(msg='Scraping Error: Info Added To Log File', duration=6000, sound=True)
@@ -41,17 +41,14 @@ def menu():
         log_utils.log('Fatal Error in %s:: Error: %s' % (base_name.title(),str(e)), log_utils.LOGERROR)
         kodi.notify(msg='Fatal Error', duration=4000, sound=True)
         quit()
-
+        
     dirlst = []
-    
-    icon = xbmc.translatePath(os.path.join('special://home/addons/script.xxxodus.artwork', 'resources/art/%s/icon.png' % filename))
-    fanarts = xbmc.translatePath(os.path.join('special://home/addons/script.xxxodus.artwork', 'resources/art/%s/fanart.jpg' % filename))
-    #dirlst.append({'name': 'Full Movies', 'url': 'http://streamingporn.xyz/movies/', 'mode': content_mode, 'icon': icon, 'fanart': fanarts, 'folder': True})
     
     for i in r:
         try:
-            name = kodi.sortX(i[1].encode('utf-8')).title()
-            dirlst.append({'name': name, 'url': i[0], 'mode': content_mode, 'icon': icon, 'fanart': fanarts, 'folder': True})
+            name = kodi.sortX(i[1].encode('utf-8'))
+            fanarts = xbmc.translatePath(os.path.join('special://home/addons/script.xxxodus.artwork', 'resources/art/%s/fanart.jpg' % filename))
+            dirlst.append({'name': name, 'url': i[0], 'mode': content_mode, 'icon': i[2], 'fanart': fanarts, 'folder': True})
         except Exception as e:
             log_utils.log('Error adding menu item %s in %s:: Error: %s' % (i[1].title(),base_name.title(),str(e)), log_utils.LOGERROR)
     
@@ -65,33 +62,38 @@ def content(url,searched=False):
 
     try:
         c = client.request(url)
-        r = dom_parser2.parse_dom(c, 'article', {'id': re.compile('post-\d+')})
-        r = [(dom_parser2.parse_dom(i, 'a', req='href'), \
+        r = dom_parser2.parse_dom(c, 'div', {'class': ['item-col','col','popup']})
+        r = [(dom_parser2.parse_dom(i, 'a', req=['href','title']), \
+              dom_parser2.parse_dom(i, 'span', {'class': 'time'}), \
+              dom_parser2.parse_dom(i, 'span'), \
               dom_parser2.parse_dom(i, 'img', req='src')) \
               for i in r if i]
-        r = [(i[0][0].attrs['href'], i[0][1].content, i[1][0].attrs['src']) for i in r]
+        r = [(i[0][0].attrs['href'], i[0][0].attrs['title'], i[1][0].content, \
+              i[2][3].content, i[3][0].attrs['src']) for i in r if i[2][3].content == 'VR']
         if ( not r ) and ( not searched ):
             log_utils.log('Scraping Error in %s:: Content of request: %s' % (base_name.title(),str(c)), log_utils.LOGERROR)
             kodi.notify(msg='Scraping Error: Info Added To Log File', duration=6000, sound=True)
+            quit()
     except Exception as e:
         if ( not searched ):
             log_utils.log('Fatal Error in %s:: Error: %s' % (base_name.title(),str(e)), log_utils.LOGERROR)
             kodi.notify(msg='Fatal Error', duration=4000, sound=True)
             quit()    
         else: pass
-
+        
     dirlst = []
     
     for i in r:
         try:
-            name = i[1].title()
+            name = kodi.sortX(i[1].encode('utf-8'))
+            name = name.title() + ' - [ %s ]' % kodi.sortX(i[2].encode('utf-8'))
             if searched: description = 'Result provided by %s' % base_name.title()
             else: description = name
             content_url = i[0] + '|SPLIT|%s' % base_name
             fanarts = xbmc.translatePath(os.path.join('special://home/addons/script.xxxodus.artwork', 'resources/art/%s/fanart.jpg' % filename))
-            dirlst.append({'name': name, 'url': content_url, 'mode': player_mode, 'icon': i[2], 'fanart': fanarts, 'description': description, 'folder': False})
+            dirlst.append({'name': name, 'url': content_url, 'mode': player_mode, 'icon': i[4], 'fanart': fanarts, 'description': description, 'folder': False})
         except Exception as e:
-            log_utils.log('Error adding menu item %s in %s:: Error: %s' % (i[1].title(),base_name.title(),str(e)), log_utils.LOGERROR)
+            log_utils.log('Error adding menu item %s in %s:: Error: %s' % (i[0].title(),base_name.title(),str(e)), log_utils.LOGERROR)
     
     if dirlst: buildDirectory(dirlst, stopend=True, isVideo = True, isDownloadable = True)
     else:
@@ -102,6 +104,7 @@ def content(url,searched=False):
     if searched: return str(len(r))
     
     if not searched:
-        search_pattern = '''rel\=['"]next['"]\s*href=['"]([^'"]+)\s*'''
-        
-        helper.scraper().get_next_page(content_mode,url,search_pattern,filename)
+        try: parse = re.findall('''<link\s*rel=['"]canonical['"]\s*href=['"]([^'"]+)''',r)[0]
+        except: parse = base_domain
+        search_pattern = '''<link\s*rel=['"]next['"]\s*href=['"]([^'"]+)'''        
+        helper.scraper().get_next_page(content_mode,url,search_pattern,filename,parse)
